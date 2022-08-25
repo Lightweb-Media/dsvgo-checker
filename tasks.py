2935416582df379c5a824ffcc2bf8e612b0add45
thinkpad laptop <agentur@3ele.de>
2022-08-25 23:17:58 +0200

    add json body

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from models.Browser import Browser
 import asyncio
-def create_task(domain):
-    runner = Browser(domain)
-    page = asyncio.get_event_loop().run_until_complete(runner.open_browser(domain))
+def create_task(data):
+    runner = Browser(data['domain'])
+    page = asyncio.get_event_loop().run_until_complete(runner.open_browser(data['domain']))
     return page
    
